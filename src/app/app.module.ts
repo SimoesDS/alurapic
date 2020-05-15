618beaeebfbc0e65e252a1dbeb2de0f61c5ba67e
Diego Simões <ssdieguinho@hotmail.com>
2020-05-15 04:34:34 -0300

    feat: add modules photo-list and photo-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http'
-
-import { PhotosModule } from './photos/photos.module';
+import { PhotoFormComponent } from './photo/photo-form/photo-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotoFormComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    PhotosModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
