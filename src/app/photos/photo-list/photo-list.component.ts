a459cdbc7a2d91d4811f658870b6ada4d0b222b4
Diego Simões <ssdieguinho@hotmail.com>
2020-05-17 01:17:36 -0300

    perf: adding delay for each key pressed in the search field

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 import { Photo } from '../photo/photo';
 
@@ -13,9 +15,14 @@ export class PhotoListComponent implements OnInit {
   photos: Photo[] = [];
   filter: string = '';
 
+  debounce: Subject<string> = new Subject<string>();
+
   constructor(private activatedRoute: ActivatedRoute) { }
   
   ngOnInit(): void {
     this.photos = this.activatedRoute.snapshot.data.photos;
+    this.debounce
+      .pipe(debounceTime(300))
+      .subscribe( filter => this.filter = filter);
   }
 }
