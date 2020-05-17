8032a5f0781bda2efdd39f3fe40b3bf567bb84e0
Diego Simões <ssdieguinho@hotmail.com>
2020-05-16 23:54:38 -0300

    feat: add search for photos

---
+++ src/app/photos/photo-list/filter-by-description.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Photo } from '../photo/photo';
+
+@Pipe({ name: 'filterByDescription' })
+export class FilterByDescription implements PipeTransform {
+    
+    transform(phostos: Photo[], descriptionQuery: string) {
+        descriptionQuery = descriptionQuery
+            .trim()
+            .toLowerCase();
+        
+        if(descriptionQuery.length) {
+            return phostos.filter( phosto => phosto
+                .description
+                .trim()
+                .toLowerCase()
+                .includes(descriptionQuery))
+        } else
+            return phostos;     
+    }
+}
